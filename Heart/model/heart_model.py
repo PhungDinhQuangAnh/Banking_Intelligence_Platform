d0cbd251d081bbde71aff82440c171605edb97cb
Phùng Đình Quang Anh <pd.quanganh.work@gmail.com>
2026-01-08 23:14:04 +0700

    Update heart_model.py

--- Heart/model/heart_model.py
+++ Heart/model/heart_model.py
@@ -15,32 +15,32 @@ def check_data(df, target):
     np.set_printoptions(suppress=True)
     num_cols = [col for col in df.select_dtypes(include=['int64', 'float64']).columns]
     print("")
-    print("🔍 THÔNG TIN CƠ BẢN:")
+    print("THÔNG TIN CƠ BẢN:")
     print(df.info())
     print("-------------------------------\n")
 
-    print("❓ GIÁ TRỊ THIẾU:")
+    print("GIÁ TRỊ THIẾU:")
     missing = df.isnull().sum()
     print(missing[missing > 0])
     print("-------------------------------\n")
 
-    print("📌 TRÙNG LẶP:", df.duplicated().sum(), "dòng")
+    print("TRÙNG LẶP:", df.duplicated().sum(), "dòng")
     print("-------------------------------\n")
 
-    print("⚠️ KIỂM TRA DỮ LIỆU:")
+    print("KIỂM TRA DỮ LIỆU:")
     for col in df.columns:
         print(f"- {col}:", df[col].unique())
     print("-------------------------------\n")
 
-    print("⚠️ KIỂM TRA PHÂN PHỐI GIÁ TRỊ CÁC CỘT:")
+    print("KIỂM TRA PHÂN PHỐI GIÁ TRỊ CÁC CỘT:")
     for col in df.columns:
         if col != target:
-            print(f"🔹 {col} value_counts():")
+            print(f"{col} value_counts():")
             print(df[col].value_counts(dropna=False))
             print("")
     print("-------------------------------\n")
 
-    print("📦 KIỂM TRA KIỂU DỮ LIỆU SAI:")
+    print("KIỂM TRA KIỂU DỮ LIỆU SAI:")
     for col in df.select_dtypes(include='object').columns:
         try:
             pd.to_numeric(df[col])
@@ -49,15 +49,15 @@ def check_data(df, target):
             pass
     print("-------------------------------\n")
 
-    print("📊 THỐNG KÊ MÔ TẢ CÁC CỘT DỮ LIỆU SỐ:")
+    print("THỐNG KÊ MÔ TẢ CÁC CỘT DỮ LIỆU SỐ:")
     pd.set_option('display.max_columns', None)
     print(df[num_cols].describe())
     print("-------------------------------\n")
 
-    print("🎯 KIỂM TRA ĐỘ MẤT CÂN BẰNG DỮ LIỆU:")
+    print("KIỂM TRA ĐỘ MẤT CÂN BẰNG DỮ LIỆU:")
     class_counts = df[target].value_counts()
     print(class_counts)
-    print("➡️ TỈ LỆ PHẦN TRĂM (%):")
+    print("TỈ LỆ PHẦN TRĂM (%):")
     print(round(df[target].value_counts(normalize=True) * 100, 2))
     print("-------------------------------\n")
 def group_median_impute(df, target_col, group_cols, invalid_zero=False):
@@ -78,7 +78,7 @@ def remove_missing_invalid(df,col):
 def check_outliers(df):
     num_cols = [col for col in df.select_dtypes(include=['int64', 'float64']).columns]
     print("")
-    print("📈 OUTLIERS (IQR method):")
+    print("OUTLIERS (IQR method):")
     for col in num_cols:
         Q1 = df[col].quantile(0.25)
         Q3 = df[col].quantile(0.75)
@@ -90,7 +90,7 @@ def check_outliers(df):
             print(f"- {col}: {len(outliers)} outliers")
     print("-------------------------------\n")
 
-    print("🧾 BIỂU ĐỒ BOXPLOT (Phát Hiện Outliers Và Độ Lệch Dữ Liệu):")
+    print("BIỂU ĐỒ BOXPLOT (Phát Hiện Outliers Và Độ Lệch Dữ Liệu):")
     df[num_cols].plot(kind='box', subplots=True, layout=(3, 3), sharex=False)
     plt.show()
     print("-------------------------------\n")
@@ -103,16 +103,16 @@ def clip_outliers(df, col):
     df[col] = df[col].clip(lower, upper)
     return df
 
-# --- Get the absolute path of the current directory ---
+# --- Lấy đường dẫn tuyệt đối của thư mục hiện tại ---
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# --- 1. Load & Explore Dataset ---
+# --- 1. Tải & Khám phá Dataset ---
 df = pd.read_csv(os.path.join(BASE_DIR, "..", "dataset", "heart_dataset.csv"))
 target = "HeartDisease"
 check_data(df, target)
 
-# --- 2. Data Preprocessing ---
-# 2_1. Handle missing or invalid values
+# --- 2. Tiền xử lý dữ liệu ---
+# 2_1. Xử lý missing và invalid values
     # print("RestingBP: ", (df['RestingBP'] == 0).sum())
     # print("Cholesterol: ", (df['Cholesterol'] == 0).sum())
 
@@ -121,22 +121,22 @@ df = remove_missing_invalid(df,"RestingBP")
 # Cholesterol
 df['AgeGroup'] = pd.cut(
     df['Age'],
-    bins=[0, 35, 55, 120],
-    labels=['Young', 'Middle', 'Old']
+    bins = [0, 35, 55, 120],
+    labels = ['Young', 'Middle', 'Old']
 )
 df = group_median_impute(df, target_col='Cholesterol', group_cols=['Sex','AgeGroup','HeartDisease'], invalid_zero=True)
 df = df.drop(columns='AgeGroup')
 
-# 2_2. Handle Outliers
+# 2_2. Xử lý Outliers
 for col in ['RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak']:
     df = clip_outliers(df, col)
 
-# 2_3. Data Partitioning
+# 2_3. Chia x, y
 x = df.drop(target,axis=1)
 y = df[target]
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=42)
 
-# 2_4. Data Transformation
+# 2_4. Biến đổi dữ liệu
 num_cols = [col for col in df.select_dtypes(include=['int64', 'float64']).columns
             if df[col].nunique() > 2]
 
@@ -155,21 +155,20 @@ ord_scaler = OrdinalEncoder(categories=ord_categories)
 nom_scaler = OneHotEncoder(sparse_output=True)
 
 preprocessor = ColumnTransformer(transformers=[
-    ("num_features",num_scaler,num_cols),
-    ("ord_features",ord_scaler,ord_cols),
-    ("nom_features",nom_scaler,nom_col)
+    ("num_features", num_scaler, num_cols),
+    ("ord_features", ord_scaler, ord_cols),
+    ("nom_features", nom_scaler, nom_col)
 ])
 
-# --- 3. Model Building ---
+# --- 3. Xây dựng mô hình ---
 model = Pipeline(steps=[
-    ("preprocessor",preprocessor),
-    ("classifier",RandomForestClassifier(max_depth=5, max_features = 'sqrt', min_samples_leaf = 4, min_samples_split = 5, n_estimators = 100))
+    ("preprocessor", preprocessor),
+    ("classifier", RandomForestClassifier(max_depth=5, max_features = 'sqrt', min_samples_leaf = 4, min_samples_split = 5, n_estimators = 100))
 ])
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
-
-# --- 4. Model Evaluation ---
+# --- 4. Đánh giá mô hình ---
 # 4.1. Evaluation Metrics
 print("")
 print(classification_report(y_test, y_pred))
@@ -188,22 +187,22 @@ plt.title("Confusion Matrix")
 plt.savefig(os.path.join(BASE_DIR, "..", "report", "heart_confusion_matrix.jpg"))
 plt.show()
 
-# 4.3. Analyze Feature Importances
+# 4.3. Phân tích feature quan trọng
 importances = model.named_steps["classifier"].feature_importances_
 feature_names = model.named_steps["preprocessor"].get_feature_names_out()
 feature_imp = pd.Series(importances, index=feature_names).sort_values(ascending=False)
 print(feature_imp)
 print("")
 
-# 4.4. Assess Model Stability Using Cross-Validation
+# 4.4. Đánh giá tính ổn định mô hình bằng Cross-Validation
 scores = cross_val_score(model, x, y, cv=5)
 print("Cross-val accuracy each fold:", scores)
 print("Mean:", scores.mean(), "Std:", scores.std())
 print("")
 
-# 4.5. Compare Accuracy Between Training and Test Sets --> Check Overfitting
+# 4.5. So sánh độ chính xác giữa bộ train và test --> Check Overfitting
 print("Train Accuracy:", model.score(x_train, y_train))
 print("Test Accuracy:", model.score(x_test, y_test))
 
-# Load Model
+# Lưu mô hình
 joblib.dump(model, "heart_model.pkl")
