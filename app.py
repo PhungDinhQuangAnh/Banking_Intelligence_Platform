e003daabeb48ab5ba2f7cb7703c97f234c7a7cf9
Phùng Đình Quang Anh <pd.quanganh.work@gmail.com>
2026-09-16 13:47:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -152,9 +152,9 @@ st.markdown("""
         @media (min-width: 1280px) {
             section[data-testid="stSidebar"] {
                 transform: none !important;
-                width: 297px !important;
-                min-width: 297px !important;
-                max-width: 297px !important;
+                width: 305px !important;
+                min-width: 305px !important;
+                max-width: 305px !important;
             }
         }
     </style>
